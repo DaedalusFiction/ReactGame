220ba9af48f458bccacc498459174ad9087e4ef5
David Sorensen <teardownthismall@gmail.com>
2021-09-11 13:23:30 -0400

    let's see if this works

---
+++ hellchopper/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+const myName = 'David'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex">
+        <p>EXPERIENCE HELLCHOPPER!</p>
+        <p>Good job, {myName}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
