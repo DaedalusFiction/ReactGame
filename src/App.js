079a1cd5d7fbce136f68b48247a04ccfddaabb9f
David Sorensen <teardownthismall@gmail.com>
2021-09-17 12:27:28 -0400

    migrated other site to this

--- src/App.js
+++ src/App.js
@@ -1,73 +1,7 @@
 import { useState } from 'react';
-import Header from './components/Header';
-import Button from './components/Button';
-import SetList from './SetList';
-import AddSong from './components/AddSong';
-
 
 function App() {
-  const [songForm, setSongForm] = useState(false)
-  const [setList, setSetList] = useState([{ 
-    id: 1,
-    artist: "The White Stripes",
-    songTitle: "The Hardest Button to Button",
-    encore: true,
-},
-{
-    id: 2,
-    artist: "The Proclaimers",
-    songTitle: "I would Be (500 miles)",
-    encore: false,
-},
-{
-    id: 3,
-    artist: "Weezer",
-    songTitle: "The Sweater Song",
-    encore: false,
-}]);
-
-// Toggle form visibility
-
-const toggleVisibility = () => {
-  setSongForm(!songForm)
-}
-
-// Delete Song from Set List
-
-const deleteSong = (id) => {
-  setSetList(setList.filter((song) => song.id !== id))
-}
-
-// Add Song to Set List
-
-const addSong = (song) => {
-  const id = 4
-  const newSong = { id, ...song }
-  setSetList([...setList, newSong])
-}
-
-// Set Song as Encore
-
-const toggleEncore = (id) => {
-  setList.map((song) => song.id === id ? {...song, encore: !song.encore} : song)
-}
-
-  return (
-    <div className="App">
-      <div className="container">
-        <Header />
-        <Button text={songForm ? "Close" : "Add"} onClick={toggleVisibility} />
-        {songForm && <AddSong addSong={addSong} />} 
-        
-
-        
-        {setList.length > 0 ?  
-          <SetList setList={setList} onDelete={deleteSong} toggleEncore={toggleEncore} /> : 
-          "No Songs to Show"
-        }
-      </div>
-    </div>
-  );
+  return <h1>Hello</h1>
 }
 
 export default App;
